a2c5545f01fe180bcaf209b7c02f1fc7ad82bafc
Tres Seaver <tseaver@palladion.com>
2013-05-06 08:55:14 -0400

    Drop support for Python 2.5 / Jython.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
